279dd4cf18e5c8690575b588e3c7a28b3da785df
viyer-research <vasyncmob@gmail.com>
2014-07-26 15:21:45 -0500

    Create ProgrammingAssignment2.R
    Assignment2 work files

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,30 @@
+# Author : Vasanth
+# R Programming 
+#################################################
+makeCacheMatrix <- function(x = matrix()) {
+#  m<-NULL
+  set<-function(y){
+    x<<-y
+    #m<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) m<<- solve
+  getmatrix<-function() m
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
+}
+
+cacheSolve <- function(x=matrix(), ...) {
+  m<-x$get()
+  
+  if(!is.null(m)){
+    message("getting cached inverse")
+    m
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
+}
